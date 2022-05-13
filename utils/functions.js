4fceea4367216e4fd2691ad733abade3ac9a81ea
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-05-14 01:46:46 +0200

    1.3.0

--- utils/functions.js
+++ utils/functions.js
@@ -5,42 +5,43 @@
 
 class func {
 
-    static commentUser(client, SteamID) {
-        //logg.send(`Got comment function, UserId: ${SteamID}`)
-        client.community.getSteamUser(SteamID, (err, user) => {
-            if (err) {
-              client.log.error(
-                `An error occurred while getting user profile: Usually private. \nSteam Response: ${err}`,
-                );
-            } else {
-                user.comment(client.config.commentText, (err) => {
-                if (err) {
-                  client.log.error(
-                    `An error occurred while commenting on user profile: comments disabled for any reason. \nSteam Response: ${err}`
-                    );
-                }
-                });
-            }
-        });
-        return;
-    }
-
-    static idler(client, shalStop, setOnline = null, addedGame = null){
-      let gamesPlayedArray = client.config.other.idleGames;
-        if (setOnline){
-          client.setPersona(1); // SteamUser.EPersonaState.Online
-        }
-        if (!shalStop) {
-          client.gamesPlayed(client.config.statusText + ' | Afk');
-        }
-        else if (shalStop){
-          if (gamesPlayedArray.lenght > 23) { //max 25 but to be sure lets keep it in 23
-            console.log('Max amount of games allowed passed')
+  static commentUser(client, SteamID) {
+    //logg.send(`Got comment function, UserId: ${SteamID}`)
+    client.community.getSteamUser(SteamID, (err, user) => {
+      if (err) {
+        client.log.error(
+          `An error occurred while getting user profile: Usually private. \nSteam Response: ${err}`,
+        );
+      } else {
+        user.comment(client.config.commentText, (err) => {
+          if (err) {
+            client.log.error(
+              `An error occurred while commenting on user profile: comments disabled for any reason. \nSteam Response: ${err}`
+            );
           }
-          if (!gamesPlayedArray.includes(client.config.statusText)) gamesPlayedArray.unshift(client.config.statusText) //https://stackoverflow.com/questions/8073673/how-can-i-add-new-array-elements-at-the-beginning-of-an-array-in-javascript
-          client.gamesPlayed(gamesPlayedArray);
-        }
+        });
       }
+    });
+    return;
+  }
+
+  static idler(client, shalStop, setOnline = null, addedGame = null) {
+    let gamesPlayedArray = client.config.other.idleGames;
+    if (setOnline) {
+      client.setPersona(1); // SteamUser.EPersonaState.Online
+    }
+    if (!shalStop) {
+      client.gamesPlayed(client.config.statusText + ' | Afk');
+    }
+    else if (shalStop) {
+      if (gamesPlayedArray.lenght > 23) { // max 25 but to be sure lets keep it in 23
+        console.log('Max amount of games allowed passed')
+        return;
+      }
+      if (!gamesPlayedArray.includes(client.config.statusText)) gamesPlayedArray.unshift(client.config.statusText) // https://stackoverflow.com/questions/8073673/how-can-i-add-new-array-elements-at-the-beginning-of-an-array-in-javascript
+      client.gamesPlayed(gamesPlayedArray);
+    }
+  }
 };
 
 module.exports = func;
