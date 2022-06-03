a8ae25d2becf1357d4339327702080b83ebb781d
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2022-06-04 01:09:25 +0200

    1.3.8

--- commands/commands.js
+++ commands/commands.js
@@ -1,6 +1,6 @@
 
-module.exports = (client) =>{ 
-    client.on("friendMessage", function(user, msg, type){
+module.exports = (client) => {
+    client.on("friendMessage", function (user, msg, type) {
         msg = msg.toLowerCase();
         if (msg == "!help") { //User Commands >------------------------------------------------------------------
             client.chatMessage(user, "/pre Main Commands");
@@ -10,22 +10,19 @@ module.exports = (client) =>{
 3- Type !test to test the chat messages
             `);
         }
-        if(msg == "!comment"){ //This will comvert the message to MAYUS and it will detect the comands like: Hello hello, HeLlO 
+        if (msg == "!comment") { //This will comvert the message to MAYUS and it will detect the comands like: Hello hello, HeLlO 
             client.chatMessage(user, client.config.other.me + "Commenting in your profile!");
             //client.log.send(`Commentig 1st to: ${user}`)
             client.functions.commentUser(client, user)
         }
-        if(msg == "hello"){ 
+        if (msg == "hello") {
             client.chatMessage(user, client.config.other.me + "Hello! Im a bot!");
         }
         if (msg == "!group") {
-            client.log.success("[Steam] | "+ user + " has been invited to " + client.config.other.groupID); //--> looks like doesnt get the log function!!!!!! //now does :D
+            client.log.success("[Steam] | " + user + " has been invited to " + client.config.other.groupID); //--> looks like doesnt get the log function!!!!!! //now does :D
             client.chatMessage(user, "https://steamcommunity.com/groups/itsxnoobx");
             client.inviteToGroup(user, client.config.other.groupID);
             client.chatMessage(user, "/pre Check your group invites here:\n" + `https://steamcommunity.com/profiles/${user}/groups/pending`);
-        //} else {
-            //client.chatMessage(steamID, "/quote Looks like you are already in the group!");
-        //};
         }
     });
 }
